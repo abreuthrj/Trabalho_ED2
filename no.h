bc1ad52430945167368c51e45c99d2e1dc87d030
Thiago Abreu <abreuthrj@gmail.com>
2021-01-18 17:44:51 -0300

    adicionado no.h

---
+++ no.h
@@ -0,0 +1,26 @@
+#include "no.cpp"
+
+using namespace std;
+
+No::No()
+{
+
+}
+
+No::No(string data, string estado, string cidade, float codigo, int casos, int mortes)
+{
+    this->prox_no = nullptr;
+    this->ant_no = nullptr;
+
+    this->data = data;
+    this->estado = estado;
+    this->cidade = cidade;
+    this->codigo = codigo;
+    this->casos = casos;
+    this->mortes = mortes;
+}
+
+No::~No()
+{
+
+}
