744f5bed7d3ae65b44c7b4de2b11c296462a122f
Thiago Abreu <abreuthrj@gmail.com>
2021-01-22 00:05:54 -0300

    permitido caminho do arquivo como parametro

--- Trabalho_ED2_2020_3.cpp
+++ Trabalho_ED2_2020_3.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-int main(char** argv, int argc)
+int main(int argc, char** argv)
 {
     string fname;
     if( argc > 1 )
-        fname = argv[0]; 
+        fname = argv[1]; 
     else
         fname = "brazil_covid19_cities.csv";
     ifstream file(fname);
