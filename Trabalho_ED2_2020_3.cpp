86048f9750d3f831a24dd132d903d4f07f2b5b89
Thiago Abreu <abreuthrj@gmail.com>
2021-01-22 00:03:13 -0300

    implementadas funcoes no menu

--- Trabalho_ED2_2020_3.cpp
+++ Trabalho_ED2_2020_3.cpp
@@ -5,13 +5,17 @@
 
 using namespace std;
 
-string fname = "brazil_covid19_cities.csv";
-ifstream file(fname);
-Lista* lista = new Lista(file);
-Lista* sublista = nullptr;
-
-int main()
+int main(char** argv, int argc)
 {
+    string fname;
+    if( argc > 1 )
+        fname = argv[0]; 
+    else
+        fname = "brazil_covid19_cities.csv";
+    ifstream file(fname);
+    Lista* lista = new Lista(file);
+    Lista* sublista = nullptr;
+
     menu(lista,sublista);
 
     return 0;
